0fa8391c8d8c1d107d775c7f8c07e0c258d3be11
Abrar Mahmood <abrarmahmood@me.com>
2018-11-17 11:35:05 +0000

    VSCode debugging working

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 interface IHomer {
-    name(): String;
+    name(shout: boolean): String;
 }
 
 
 class Homer implements IHomer {
-    name(){
-        return 'Homer Simpson';
+    name(shout: boolean){
+        if (shout) {
+            return 'Homer Simpson!!!!';
+        }
+        return 'Homer Simpson ok';
     }
 }
 
 const instance = new Homer();
-console.log(instance.name());
+console.log(instance.name(false));
