607087e233e6f6e1f580ed8d999605fdd355e832
Abrar Mahmood <abrarmahmood@me.com>
2018-11-24 19:37:34 +0000

    Updated tsconfig.json to work work with nodejs as the target

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ var path = require('path');
 module.exports = {
     entry: './src/index.ts',
     devtool: 'inline-source-map',
+    target: 'node',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     resolve: {
-      extensions: ['.ts'] //resolve all the modules other than index.ts
+        extensions: ['.ts', '.js', '.json'] //resolve all the modules other than index.ts
     },
     module: {
         rules: [
